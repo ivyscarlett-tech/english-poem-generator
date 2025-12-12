39d990de579bbae567adff2dd99f3fc81e3da36c
ivyscarlett-tech <ivy.o.ansah@gmail.com>
2025-12-12 20:54:23 +0000

    Added the basic JavaScript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("Hello World");
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: ["Because I love you"],
+    autoStart: true,
+    delay: 10,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
